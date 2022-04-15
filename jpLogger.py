3f9b2c984aa7d8e19d3686cd22f0da4b3e811f67
Jose Juan Jaramillo Polo <josejuan.jaramillopolo@gmail.com>
2022-04-15 02:42:53 -0500

    V1.1.0

--- jpLogger.py
+++ jpLogger.py
@@ -1,17 +1,18 @@
 # =============================================================================
 # File Name: jpLogger.py
 # =============================================================================
-# Purpose:          A wrapper class of the logging library from the Python 
+# Purpose:          An inherited class of the logging library from the Python 
 #                   standard library. It aims to simplify and speed up the
-#                   instantiaton of a logging object.
+#                   instantiation of a logging object.
 #
 # Author:           Jose Juan Jaramillo Polo
-# Version:          1.0
+# Version:          1.1
 # Notes:             
 # =============================================================================
 
-from logging.handlers import RotatingFileHandler    # For better file logs management| RotatingFileHandler instead of FileHandler
-import logging, os                                  # Needed to check if a log files was already created before.
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 import enum
 
 class displayMode(enum.Enum):
@@ -22,20 +23,19 @@ class displayMode(enum.Enum):
     fileOnly        = 2
     fileAndConsole  = 3
 
-class jpLogger():
+class jpLogger(logging.getLoggerClass()):
     """
-    A wrapper class of the logging library from the Python standard library.
-    It aims to simplify the way a logging object is created by inclding a 
-    a handler for displaying logs in console and in a file as part of its 
+    An inherited class of the logging library from the Python standard library.
+    It aims to simplify the way a logging object is created by including a 
+    a handler for displaying logs in the console and in a file as part of its 
     constructor.
     It also includes default values in the constructor as a matter of demo purposes.
-
     """
 
-    def __init__(self, appName="unknown", logFileName = "noName.log", logLevel=logging.INFO, mode=displayMode.fileAndConsole, maxFiles=5, maxFileSize=5*1024*1024) -> None:       
+    def __init__(self, appName="unknown", logFileName="noName.log", logLevel=logging.DEBUG, logDisplayMode=displayMode.fileAndConsole, maxFiles=5, maxFileSize=5*1024*1024) -> None:
         """
         Instantiate a logging object to simplify and speed up the creation of a
-        logging object that includes (or not depending on the mode var value) a 
+        logging object that includes (or not depending on the logDisplayMode var value) a 
         handler to display the log messages in the console and to log the log 
         messages in a file using a rotating file handler (the rotating file 
         handler allows to store more than one log file and shift their names as
@@ -45,7 +45,7 @@ class jpLogger():
         Parameters
         ----------
         appName : str, optional
-            The name of the app that is calling/instantianing a jpLogger object.
+            The name of the app that is calling/instantiating a jpLogger object.
             This var is used as part of the format.
         logFileName : str, optional
             The path (if not included, log file will be created next to this script)
@@ -53,68 +53,81 @@ class jpLogger():
         logLevel : int, optional
             The level of logging to use.
             Available enum options: logging.INFO, logging.DEBUG, etc.
-        mode : int, optional
+        logDisplayMode : int, optional
             Specify the output target that could be console of log file.
-            Availabel options come from displayMode enum class.
+            Available options come from displayMode enum class.
         maxFiles : int, optional
-            The max number of log files to be stored and handled by the 
+            The maximum number of log files to be stored and handled by the 
             rotating file handler.
         maxFileSize : int, optional
-            The maximum size of the log file before a new one needs to be created.
-        """
-        self.logger = logging.getLogger(appName) # assinging it a name (reference)
-        self.logger.setLevel(logLevel)           # Despite the config (setLevel) of handlers, 
-                                                 # this controls the general-log-level to be showed 
-                                                 # All lower levels will be included.
-      
-        if(mode == displayMode.consoleOnly) or (mode == displayMode.fileAndConsole):
-            #Creating a printer (in cmd) log handler
-            logsToShowInaShell = logLevel #all lower levels will be included 
-            logsToShowInaShell_handler = logging.StreamHandler()
-            logsToShowInaShell_handler.setLevel(logsToShowInaShell)
-            shell_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-            logsToShowInaShell_handler.setFormatter(shell_format)
-            self.logger.addHandler(logsToShowInaShell_handler)   
-            self.logger.debug('Logger CMD handler has been initialized successfully!')
+            The maximum size (in bytes) of the log file before a new one needs to be created.
+        """ 
+        super().__init__(appName)   # Assigning it a name (reference)
 
-        if(mode == displayMode.fileOnly) or (mode == displayMode.fileAndConsole):
-            #Creating a log file handler
-            logsToInsertInFile = logLevel #all lower levels will be included 
-            should_roll_over = os.path.isfile(logFileName)  # Checks if there is a log file (logFileName) already created, 
-                                                            # if so it doRollover() method will rotates the files, this
-                                                            # we will have a new file on every run and the oldest one will
-                                                            # be deleted.
-            logsToInsertInFile_handler = RotatingFileHandler(logFileName, mode='a', maxBytes=maxFileSize, 
+        self.setLevel(logLevel)     # Despite the config (setLevel) of handlers, 
+                                    # this controls the general-log-level to be showed 
+                                    # All lower levels will be included.      
+
+        if(logDisplayMode == displayMode.consoleOnly) or (logDisplayMode == displayMode.fileAndConsole):
+            consoleLogLevel = logLevel 
+            #consoleLogFormat = logging.Formatter('%(message)s')
+            consoleLogFormat = logging.Formatter('%(asctime)s - %(levelname)s [ref: %(module)s %(funcName)s(%(lineno)d)] - %(message)s')
+            consoleLogHandler = logging.StreamHandler()
+            consoleLogHandler.setLevel(consoleLogLevel)
+            consoleLogHandler.setFormatter(consoleLogFormat)
+            self.addHandler(consoleLogHandler)   
+            #self.debug('CMD handler has been added successfully!')
+
+        if(logDisplayMode == displayMode.fileOnly) or (logDisplayMode == displayMode.fileAndConsole):
+            fileLogLevel = logLevel 
+            fileLogFormat = logging.Formatter('%(asctime)s [tickCount: %(msecs)d] [%(processName)s %(process)d] [%(threadName)s %(thread)d] - %(levelname)s [ref: %(module)s %(funcName)s(%(lineno)d)] - %(message)s')
+            self.create_directory_from_file_path(logFileName)
+            fileLogHandler = RotatingFileHandler(logFileName, mode='a', maxBytes=maxFileSize, 
                                         backupCount=maxFiles, encoding=None, delay=0)
-            if should_roll_over:  # log already exists, roll over!
-                logsToInsertInFile_handler.doRollover()
-            logsToInsertInFile_handler.setLevel(logsToInsertInFile)
-            file_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-            logsToInsertInFile_handler.setFormatter(file_format)
-            self.logger.addHandler(logsToInsertInFile_handler)
-            self.logger.debug('Logger FILE handler has been initialized successfully!')
-    
-        #Use this as an example to use the logger
-        self.logger.info('Logger has been initialized successfully!')
+            fileLogHandler.setLevel(fileLogLevel)
+            fileLogHandler.setFormatter(fileLogFormat)
+            shouldRollOver = os.path.isfile(logFileName)    # Checks if there is a log file (logFileName) already created, 
+                                                            # if so the doRollover() method will rotates the files, this
+                                                            # we will create a new file on every run and the oldest one will
+                                                            # be deleted.
+            if shouldRollOver:  # Log file already exists, roll over!
+                fileLogHandler.doRollover()
+            self.addHandler(fileLogHandler)
+            #self.debug('FILE handler has been added successfully!')
 
-    
-    def debug(self, str):
-        self.logger.debug(str)
+            #self.debug('Logger has been initialized successfully!')
 
-    def info(self, str):
-        self.logger.info(str)
+    def create_directory_from_file_path(self, file_path):
+        """
+        Extracts the directory of a path (ignoring the file and its extension)
+        and tries to create the directory. 
+        Parameters
+        ----------
+        file_path : str
+            The full path of the log file from where this 
+            method will take the directory path and try to
+            create the directory. 
+        """
 
-    def error(self, str):
-        self.logger.error(str)
+        # Extract the directory path from the file path
+        directory_path = os.path.dirname(file_path)
+        if directory_path == "":
+            print(f"Logging directory '{directory_path}' does not need to be created. \n")
+            return
 
-    def warning(self, str):
-        self.logger.warning(str)
-
-    def critical(self, str):
-        self.logger.critical(str)
+        # Check if the directory already exists
+        if not os.path.exists(directory_path):
+            try:
+                # Create the directory
+                os.makedirs(directory_path)
+                print(f"Logging directory '{directory_path}' created successfully. \n")
+            except OSError as e:
+                print(f"Error creating logging directory '{directory_path}': {e} \n")
+        else:
+            print(f"Logging directory '{directory_path}' already exists. \n")
 
 if __name__ == "__main__":
-    demoLogger = jpLogger("Demo App ", "demoFile.log", logging.DEBUG, displayMode.fileAndConsole, 10, 5*1024*1024)
+    demoLogger = jpLogger("Demo App ", "demoFile.log", logging.DEBUG, displayMode.fileAndConsole, 7, 5*1024*1024)
     demoLogger.info("Demo Logger")
     demoLogger.debug("Demo Logger")
     demoLogger.warning("Demo Logger")
